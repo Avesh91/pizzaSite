38f12c700c5ab1f031cd02738f022e99488b7d2b
Avesh91 <aveshansari7474@gmail.com>
2022-10-04 13:33:34 -0700

    Create React app

---
+++ src/components/Footer.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+
+
+            <section className="footer">
+            <div className='f-data'>
+                <div className="box-container">
+                    <div className="box">
+                        <h3>locations</h3>
+                        <a >india</a>
+                        <a >japan</a>
+                        <a >russia</a>
+                        <a >USA</a>
+                        <a >france</a>
+                    </div>
+                    <div className="box">
+                        <h3>quick links</h3>
+                        <a >home</a>
+                        <a >dishes</a>
+                        <a >about</a>
+                        <a >menu</a>
+                        <a >reivew</a>
+                        <a >order</a>
+                    </div>
+                    <div className="box">
+                        <h3>contact info</h3>
+                        <a >+123-456-7890</a>
+                        <a >+111-222-3333</a>
+                        <a >aveshansari7474@gmail.com</a>
+                        <a >aveshansari7474@gmail.com</a>
+                        <a >indore, india - 400104</a>
+                    </div>
+                    <div className="box">
+                        <h3>follow us</h3>
+                        <a >facebook</a>
+                        <a >twitter</a>
+                        <a >instagram</a>
+                        <a >linkedin</a>
+                    </div>
+                </div>
+                </div>
+                <div className="credit" > copyright @ 2022 by <span>mr. Avesh</span> </div>
+
+            </section>
+
+        </div>
+    )
+}
+
+export default Footer
